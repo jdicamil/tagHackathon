e0647b1a8c96ebf469d64c186f87ee0507d570d5
Dario Laverde <dario@nycjava.net>
2014-01-05 13:30:45 -0800

    BT and M2X!

--- app project/m2x-android-sdk-testapp/src/com/att/m2x/testapp/BluetoothChat.java
+++ app project/m2x-android-sdk-testapp/src/com/att/m2x/testapp/BluetoothChat.java
@@ -146,20 +146,9 @@ public class BluetoothChat extends Activity {
 
         // Initialize the send button with a listener that for click events
 //        mSendButton = (Button) findViewById(R.id.button_send);
-        mSendButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Send a message using content of the edit text widget
-//                TextView view = (TextView) findViewById(R.id.edit_text_out);
-//                String message = view.getText().toString();
-//                sendMessage(message);
-            }
-        });
 
         // Initialize the BluetoothChatService to perform bluetooth connections
         mChatService = new BluetoothChatService(this, mHandler);
-
-        // Initialize the buffer for outgoing messages
-        mOutStringBuffer = new StringBuffer("");
     }
 
     @Override
